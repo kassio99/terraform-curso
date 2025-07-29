8f119f9d901afc9e0f7508b23ef6737718b81586
Kassio Ogliari Ruaro <kassio.ruaro@orsegups.com.br>
2025-07-28 22:16:32 -0300

    EC2

--- main.tf
+++ main.tf
@@ -5,8 +5,18 @@ terraform {
   }
 }
 
-provider "aws"  {
-    profile = "pessoal"
-    shared_credentials_file = "~/.aws/credentials"
-    region = "us-eat-1"
+provider "aws" {
+  region  = "us-east-1"
+  profile = "pessoal"
+}
+
+resource "aws_instance" "ec2_teste" {
+  ami                         = "ami-0150ccaf51ab55a51"
+  instance_type               = "t2.micro"
+  key_name                    = "KP-Pessoal-East_1"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ec2-teste-pessoal"
+  }
 }
